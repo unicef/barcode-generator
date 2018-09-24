f6760d55e107d1009bfbe92757ba5c1ce69e4c4a
merlos <jmerlostevar@unicef.org>
2018-09-24 14:34:04 -0400

    + Added the logic to the buildPayload: if the batch is not included, then dates are neither. + on the barcode dates are now: yymmdd (earlier yyyy-mm-dd)

--- src/app/suplier-barcode.ts
+++ src/app/suplier-barcode.ts
@@ -7,24 +7,40 @@ export class SuplierBarcode extends Barcode {
   public materialNumber: string ="";
   public quantity: string ="";
   public batchNumber: string ="";
-  public manufacturingDate: string ="";
-  public expiryDate: string ="";
+  public manufacturingDate: string;
+  public expiryDate: string;
 
   constructor() {
     super();
   }
 
-  //
-  buildPayload() : string {
-    return (this.poNumber + "~"
-      + this.itemNumber + "~"
-      + this.materialNumber + "~"
-      + this.quantity + "~"
-      + this.batchNumber + "~"
-      + this.manufacturingDate + "~"
-      + this.expiryDate);
+  // Get a string in the format 'yyyy-mm-dd'
+  // and converts it into the format 'ddmmyy'
+  dateToBarcodeString( date: string) : string {
+    let dateComponents = date.match(/(\d+)\-(\d+)-(\d+)/);
+    return dateComponents[1].substring(2,4) +
+      dateComponents[2].padStart(2,'0') +
+      dateComponents[3].padStart(2,'0')
   }
 
+  //
+  //
+  buildPayload() : string {
+    let payload = this.poNumber + "~"
+      + this.itemNumber + "~"
+      + this.materialNumber + "~"
+      + this.quantity
+    if this.batchNumber.length > 0 {
+      payload += "~"
+        + this.batchNumber + "~"
+        + this.dateToBarcodeString(this.manufacturingDate) + "~"
+        + this.dateToBarcodeString(this.expiryDate)
+    }
+    return payload
+  }
+
+  //
+  //
   barcodeType() : string {
     return 'suplier'
   }
